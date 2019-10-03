7b449f84de8e30a37275425b9de213aba62cf0f5
fengyaxin <1620817776@qq.com>
2019-10-03 11:59:55 +0800

    结巴测试

--- 分词/JiebaFenci.py
+++ 分词/JiebaFenci.py
@@ -1,37 +1,3 @@
-# import jieba
-#
-# def get_content(path):
-#     with open(path,'r',encoding='gbk',errors='ignore') as f:
-#         content = ''
-#         for l in f:
-#             l = l.strip()
-#             content += 1
-#         return content
-#
-# def get_TF(words,topK=10):
-#     tf_dic = {}
-#     for w in words:
-#         tf_dic[w] = tf_dic.get(w,0)+1
-#     return sorted(tf_dic.items(),key = lambda x:x[1],reversed=True)[:topK]
-#
-# def main():
-#     import glob
-#     import random
-#     import  jieba
-#
-#     files = glob.glob('‎/Users/fengxiaolan/JupyterSpace/C000008/*.txt')
-#     corpus = [get_content(x) for x in files]
-#
-#     sample_inx = random.randint(0,len(corpus))
-#
-#     split_words = list(jieba.cut(corpus[sample_inx]))
-#     print('样本之一：'+corpus[sample_inx])
-#     print('样本分词效果：'+'/'.join(split_words))
-#     print('样本的topK(10)'+str(get_TF(split_words)))
-#
-# main()
-
-
 def get_content(path):
     with open(path, 'r', encoding='gbk', errors='ignore') as f:
         content = ''
